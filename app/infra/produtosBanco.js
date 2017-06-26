c78f5718129513b5a6d298fd588ab9f2cad6b809
Willian Krause <krause.willian@gmail.com>
2017-06-26 15:18:35 -0300

    Aula 2, Lição 21

--- app/infra/produtosBanco.js
+++
@@ -1,9 +0,0 @@
-module.exports = function() {
-    return function(connection) {
-        this.lista = function(callback) {
-            connection.query('select * from livro', callback);
-        };
-        return this;
-    }
-
-};
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -2,7 +2,7 @@ module.exports = function (app) {
     app.get('/produtos', function (req, res) {
         var conn = app.infra.connectionFactory();
 
-        var produtosBanco = new app.infra.produtosBanco(conn);
+        var produtosBanco = new app.infra.ProdutosDAO(conn);
         produtosBanco.lista(function (err, result) {
             if (err) {
                 console.log(err);
