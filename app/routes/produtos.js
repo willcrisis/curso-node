0434466fa73e7a26e825e0816a0bb5abcd7eacea
Willian Krause <krause.willian@gmail.com>
2017-06-26 15:11:46 -0300

    Aula 2, Lição 19

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -2,7 +2,7 @@ module.exports = function (app) {
     app.get('/produtos', function (req, res) {
         var conn = app.infra.connectionFactory();
 
-        var produtosBanco = app.infra.produtosBanco(conn);
+        var produtosBanco = new app.infra.produtosBanco(conn);
         produtosBanco.lista(function (err, result) {
             if (err) {
                 console.log(err);
