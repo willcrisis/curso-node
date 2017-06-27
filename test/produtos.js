c1531086b52bee096a5eef6f694a4dcccf1cf4ed
Willian Krause <krause.willian@gmail.com>
2017-06-27 14:16:48 -0300

    Aula 7, Lição 3

--- test/produtos.js
+++ test/produtos.js
@@ -1,5 +1,26 @@
+var http = require('http');
+
 describe('#ProdutosController', function() {
-    it('#lista como json', function() {
-        console.log('Teste de listagem');
+
+    it('#lista como json', function(finalizar) {
+        var config = {
+            hostname: 'localhost',
+            port:3000,
+            path:'/produtos',
+            headers: {
+                'Accept':'application/json'
+            }
+        };
+
+        http.get(config,function(res){
+            if (res.statusCode == 200) {
+                console.log("Status correto");
+            }
+            if (res.headers['content-type'] == 'application/json; charset=utf-8') {
+                console.log('content type correto');
+            }
+
+            finalizar()
+        });
     })
 });
