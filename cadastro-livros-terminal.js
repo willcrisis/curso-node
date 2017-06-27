f8c0fc93af15818a3b9ee1ef014cf2aa51913a4d
Willian Krause <krause.willian@gmail.com>
2017-06-27 13:30:54 -0300

    Aula 4, Lição 8

---
+++ cadastro-livros-terminal.js
@@ -0,0 +1,27 @@
+var http = require('http');
+
+var config = {
+    hostname: 'localhost',
+    port:3000,
+    path:'/produtos',
+    method: 'post',
+    headers: {
+        'Accept':'application/json',
+        'Content-type':'application/json'
+    }
+};
+
+var client = http.request(config,function(res){
+    console.log(res.statusCode);
+    res.on('data',function(body){
+        console.log("" + body);
+    });
+});
+
+var produto = {
+    titulo: 'Incluindo via JSON',
+    descricao: 'Teste de inclusão',
+    valor: '100.00'
+};
+
+client.end(JSON.stringify(produto));
